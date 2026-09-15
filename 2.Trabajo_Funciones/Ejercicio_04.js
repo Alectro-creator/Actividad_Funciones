b46bc83faa9362fda384e88b022bbd787b0b7993
Alectro-creator <alectrolegendario@gmail.com>
2026-09-15 16:22:03 -0500

    Update Ejercicio_04.js

--- 2.Trabajo_Funciones/Ejercicio_04.js
+++ 2.Trabajo_Funciones/Ejercicio_04.js
@@ -1,12 +1,9 @@
-// Función Expresada
-// Función Flecha
-
 const prompt = require("prompt-sync")();
 
 function calcularEdadPerro(edadHumana) {
     return edadHumana * 7;
 }
 
-let edadHumana = prompt("Ingrese la edad en años humanos: ");
+let edadHumana = Number(prompt("Ingrese la edad en años humanos: "));
 
 console.log(calcularEdadPerro(edadHumana));
