35d256f24d813dc0d394061d9e0b45df075d91aa
Alectro-creator <alectrolegendario@gmail.com>
2026-09-15 16:49:44 -0500

    Update Ejercicio_09.js

--- 2.Trabajo_Funciones/Ejercicio_09.js
+++ 2.Trabajo_Funciones/Ejercicio_09.js
@@ -1,13 +1,11 @@
 const prompt = require("prompt-sync")();
 
 // Math.PI representa el valor de π.
+
 function calcularCircunferencia(radio) {
     return 2 * Math.PI * radio;
 }
 
 let radio = Number(prompt("Ingrese el radio del círculo: "));
 
-//2 × π × 5 ≈ 31.42
-
-console.log(calcularCircunferencia(radio));
 console.log(calcularCircunferencia(radio).toFixed(2));
